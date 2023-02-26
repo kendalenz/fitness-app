ea112452caad6c104b6d461f84194c32286239d5
Kendal Enz <kendal.enz@gmail.com>
2023-02-26 16:27:54 -0500

    fixed broken sponsor images

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -89,14 +89,15 @@ const Home = ({ setSelectedPage }: Props) => {
         <div className="h-[150px] w-full bg-primary-100 py-10">
           <div className="mx-auto w-5/6">
             <div className="flex w-3/5 items-center justify-between gap-8">
-              <img alt="redbull-sponsor" src={SponsorRedBull} />
-              <img alt="forbes-sponsor" src={SponsorForbes} />
-              <img alt="fortune-sponsor" src={SponsorFortune} />
+              <img alt="redbull-sponsor" src={SponsorRedBull}></img>
+              <img alt="forbes-sponsor" src={SponsorForbes}></img>
+              <img alt="fortune-sponsor" src={SponsorFortune}></img>
             </div>
           </div>
         </div>
       )}
     </section>
+    
   );
 };
 
