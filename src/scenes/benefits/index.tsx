f3fad074877eca5a5703db4f651fd357f2fcb6e0
Kendal Enz <kendal.enz@gmail.com>
2023-03-04 08:19:24 -0500

    working on Benefits section

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -1,28 +1,60 @@
 import HText from "@/shared/HText";
-import { SelectedPage } from "@/shared/types";
+import { BenefitType, SelectedPage } from "@/shared/types";
 import { HomeModernIcon, UserGroupIcon, AcademicCapIcon } from "@heroicons/react/24/solid";
 import { motion } from "framer-motion";
+import Benefit from "./Benefit";
+
+const benefits: Array<BenefitType> = [
+  {
+    icon: <HomeModernIcon className="h-6 w-6" />,
+    title: "State of the Art Facilities",
+    description: "Blah blah blah blah blah blah blah blah blah"
+  },
+  {
+    icon: <UserGroupIcon className="h-6 w-6" />,
+    title: "100's of Diverse Classes",
+    description: "Blah blah blah blah blah blah blah blah blah"
+  },
+  {
+    icon: <AcademicCapIcon className="h-6 w-6" />,
+    title: "Pro Trainers",
+    description: "Blah blah blah blah blah blah blah blah blah"
+  }
+]
 
 type Props = {
-    setSelectedPage: (value: SelectedPage) => void;
+  setSelectedPage: (value: SelectedPage) => void;
 };
 
 const Benefits = ({setSelectedPage}: Props) => {
   return (
     <section
-        id="benefits"
-        className="mx-auto min-h-full w-5/6 py-20"
+      id="benefits"
+      className="mx-auto min-h-full w-5/6 py-20"
     >
-        <motion.div
-          onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
-        >
-          <div>
-           <HText>MORE THAN JUST A GYM.</HText>
-           <p className="my-5 text-sm">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
+      >
+        {/* Header */}
+        <div className="md:my-5 md:my-3/5">
+          <HText>MORE THAN JUST A GYM.</HText>
+          <p className="my-5 text-sm">
             We provide world class fitness equipment, trainers and classes to get you to your ultimate fitness goals with ease. We provide blah blah blah to all our members.  
-           </p>
-          </div>
-        </motion.div>
+          </p>
+        </div>
+        {/* Benefits */}
+        <div className="md:flex items-center justify-between gap-8 mt-5">
+          {benefits.map((benefit: BenefitType) => (
+            <Benefit
+              key={benefit.title}
+              icon={benefit.icon}
+              title={benefit.title}
+              description={benefit.description}
+              setSelectedPage={setSelectedPage}
+            />
+          ))}
+        </div>
+      </motion.div>
     </section>
   );
 };
