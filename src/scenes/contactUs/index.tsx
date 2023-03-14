291be27730abadd507611d8d6faf013f642200ad
Kendal Enz <kendal.enz@gmail.com>
2023-03-14 19:51:17 -0400

    started contact us section

---
+++ src/scenes/contactUs/index.tsx
@@ -0,0 +1,21 @@
+import { SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
+import { useForm } from "react-hook-form";
+import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png";
+
+type Props = {
+    setSelectedPage: (value: SelectedPage) => void;
+}
+
+const ContactUs = ({ setSelectedPage }: Props) => {
+  return (
+    <section id="contactus" className="mx-auto w-5/6 pt-24 pb-32">
+      <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}>
+        {/* Header */}
+
+      </motion.div>
+    </section>
+  )
+}
+
+export default ContactUs;
