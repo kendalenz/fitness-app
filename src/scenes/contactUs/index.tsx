8bcbf418ecef87405b3c943509f71851607d34f4
Kendal Enz <kendal.enz@gmail.com>
2023-03-14 21:46:03 -0400

    working on contact form

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -2,17 +2,84 @@ import { SelectedPage } from "@/shared/types";
 import { motion } from "framer-motion";
 import { useForm } from "react-hook-form";
 import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png";
+import HText from "@/shared/HText";
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
 }
 
 const ContactUs = ({ setSelectedPage }: Props) => {
+  const inputStyles = `w-full rounded-lg bg-primary-300 
+  px-5 py-3 placeholder-white`;
+
+  const {
+   register, 
+   trigger, 
+   formState: { errors }
+  } = useForm();
+
+  const onSubmit = async(e: any) => {
+    const isValid = await trigger();
+    if (!isValid) {
+      e.preventDefault();
+    }
+  };
+
   return (
     <section id="contactus" className="mx-auto w-5/6 pt-24 pb-32">
       <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}>
         {/* Header */}
+        <motion.div
+          className="md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <HText>
+            <span className="text-primary-500">JOIN NOW</span> TO GET IN SHAPE
+          </HText>
+          <p className="my-5">
+            Blah blahhhhhhhhhhh
+            Blahhhhh
+          </p>
 
+        </motion.div>
+        {/* Form and Image */}
+        <div className="mt-10 justify-between gap-8 md:flex">
+          <motion.div className="mt-10 basis-3/5 md:mt-0"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+          >
+            <form
+              target="_blank"
+              onSubmit={onSubmit}
+              action="https://formsubmit.co/el/vezeri"
+              method="POST"
+            >
+              <input
+                className={inputStyles}
+                type="text"
+                placeholder="NAME"
+                {...register("name", {
+                    required: true,
+                    maxLength: 100,
+                })}
+              >
+              </input>
+            </form>
+          </motion.div>
+        </div>
       </motion.div>
     </section>
   )
