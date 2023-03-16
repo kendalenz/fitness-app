1b67d104b9d3b879c6d97c90453a57b7d7968a98
Kendal Enz <kendal.enz@gmail.com>
2023-03-16 14:50:03 -0400

    form completed

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -9,7 +9,7 @@ type Props = {
 }
 
 const ContactUs = ({ setSelectedPage }: Props) => {
-  const inputStyles = `w-full rounded-lg bg-primary-300 
+  const inputStyles = `mb-5 w-full rounded-lg bg-primary-300 
   px-5 py-3 placeholder-white`;
 
   const {
@@ -64,7 +64,7 @@ const ContactUs = ({ setSelectedPage }: Props) => {
             <form
               target="_blank"
               onSubmit={onSubmit}
-              action="https://formsubmit.co/el/vezeri"
+              action="https://formsubmit.co/kendal.enz@gmail.com"
               method="POST"
             >
               <input
@@ -83,7 +83,67 @@ const ContactUs = ({ setSelectedPage }: Props) => {
                   {errors.name.type === "maxLength" && "Max length is 100 characters."}
                 </p>
               )}
+              <input
+                className={inputStyles}
+                type="text"
+                placeholder="EMAIL"
+                {...register("email", {
+                    required: true,
+                    pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                })}
+              >
+              </input>
+              {errors.email && (
+                <p className="mt-1 text-primary-500">
+                  {errors.email.type === "required" && "This field is required."}
+                  {errors.email.type === "pattern" && "Invalid email address."}
+                </p>
+              )}
+
+              <textarea
+                className={inputStyles}
+                rows={4}
+                cols={50}
+                placeholder="MESSAGE"
+                {...register("message", {
+                    required: true,
+                    maxLength: 2000,
+                })}
+              >
+              </textarea>
+              {errors.message && (
+                <p className="mt-1 text-primary-500">
+                  {errors.message.type === "required" && "This field is required."}
+                  {errors.message.type === "maxLength" && "Max length is 2000 characters."}
+                </p>
+              )}
+              <button
+                type="submit"
+                className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"
+              >
+                SUBMIT
+              </button>
             </form>
+          </motion.div>
+          <motion.div
+            className="relative mt-16 basis-2/5 md:mt-0"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+          >
+            <div className="md:before:content-evolvetext w-full before:absolute before:-bottom-20 before:-right-10 before:z-[-1]">
+              <img
+                className="w-full"
+                alt="contact-us-page-graphic"
+                src={ContactUsPageGraphic}
+              >
+              </img>
+            </div>
           </motion.div>
         </div>
       </motion.div>
